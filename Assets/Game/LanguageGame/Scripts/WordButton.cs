3df7c9e77b9e0a3c3d90944f9137be51b9a9f6e7
Raptor <panraptor13@gmail.com>
2025-07-03 18:44:45 +0200

    wrong answer

--- Assets/Game/LanguageGame/Scripts/WordButton.cs
+++ Assets/Game/LanguageGame/Scripts/WordButton.cs
@@ -3,12 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
+
 
 public class WordButton : MonoBehaviour
 {
     [SerializeField] private WordDisplay display;
     [SerializeField] private int wordNumber;
     [SerializeField] private Text textDisplay;
+    [SerializeField] private Transform parent;
 
     private void Start()
     {
@@ -20,6 +23,10 @@ public class WordButton : MonoBehaviour
         if (wordNumber == display.chosenNumber)
         {
             display.RightNumber();
+        }
+        else
+        {
+            parent.DOPunchRotation(Vector3.up * 5f, 1f);
         }
     }
 }
